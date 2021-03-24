ba73acaccc6712044f0b77876da00671a6ced561
herberthsfc <76819767+herberthsfc@users.noreply.github.com>
2021-03-24 14:09:26 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1499,9 +1499,9 @@ client.on('group-participants-update', async (anu) => {
 					teks += '\n\n'
 					teks += `「 *${groupMembers.length} ᴹᵉᵐᵇʳᵒˢ* 」\n`
 					for (let mem of groupMembers) {
-						teks += `*💉├* @${mem.jid.split('@')[0]}\n`
+						teks += `💉├ @${mem.jid.split('@')[0]}\n`
+						teks += '      ╰╼❥ ❲ *_HDBOT.exe_* ❳'
 						members_id.push(mem.jid)
-					teks += '╰╼❥ ❲ *_HDBOT.exe_* ❳'
 					}
 					mentions(teks, members_id, true)
 					break
