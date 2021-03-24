fc88f2296e27a4897d0f12a98cc042b2df3af7d5
herberthsfc <76819767+herberthsfc@users.noreply.github.com>
2021-03-24 13:12:26 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1496,9 +1496,10 @@ client.on('group-participants-update', async (anu) => {
 					if (!isGroupAdmins) return reply(mess.only.admin)
 					members_id = []
 					teks = (args.length > 1) ? body.slice(8).trim() : ''
+					teks += `  Total : ${groupMembers.length}\n`
 					teks += '\n\n'
 					for (let mem of groupMembers) {
-						teks += `*#* @${mem.jid.split('@')[0]}\n`
+						teks += `*╠➥* @${mem.jid.split('@')[0]}\n`
 						members_id.push(mem.jid)
 					}
 					mentions(teks, members_id, true)
